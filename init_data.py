7211240725285c41bed95f9bb3e8af6e144922b8
Jethro <jethro.swanson@yahoo.com>
2024-12-02 18:47:27 -0600

    added updated holidays

--- init_data.py
+++ init_data.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     # Normalizes data and converts non numeric columns into numeric by expanding each element in them to a new unique column
     # NOTE: This takes a long time, and only needs to be done once as date creation is seperate, feel free to comment out if you have already
     #   generated updated_fires_db.sqlite and just run the other two faster scripts
-    init_data_reformat_data.update_db()
+    #init_data_reformat_data.update_db()
     # Creates sql script to populate basic date and holiday info
     init_data_create_sql.main()
     # Creates a new 'Calendar' table in the db with the prev sql script and populates in with weights found in the weights json
